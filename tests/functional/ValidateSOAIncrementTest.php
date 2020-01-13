f3b69f7e52d512a60802ee9fd3ba8e83f8e96e2f
Tristan <t.siebers@exonet.nl>
2020-01-13 15:27:31 +0100

    Test SOA increment

--- tests/functional/ValidateSOAIncrementTest.php
+++ tests/functional/ValidateSOAIncrementTest.php
@@ -10,8 +10,24 @@ class ValidateSOAIncrementTest extends TestCase
 {
     public function testCreateSoaIncrementZone() : void
     {
-        $powerdns = new Powerdns('127.0.0.1', 'apiKey');
+        $powerdns = new Powerdns('127.0.0.1', 'secretExoDns');
+        $zone = $powerdns->createZone('soa-increment.test', ['ns1.powerdns-php.', 'ns2.powerdns-php.']);
+        $result = $zone->create('test', RecordType::A, '127.0.0.1', 3600);
+
+        $this->assertTrue($result);
+    }
+
+    /**
+     * @depends testCreateSoaIncrementZone
+     */
+    public function testSoaIncrement(): void
+    {
+        $powerdns = new Powerdns('127.0.0.1', 'secretExoDns');
         $zone = $powerdns->zone('soa-increment.test');
-        $zone->create('test', RecordType::A, '127.0.0.1', 3600);
+        $soaData = $zone->get(RecordType::SOA);
+
+        $expectedSoa = sprintf('ns1.powerdns-php. hostmaster.powerdns-php. %s02 10800 3600 604800 3600', date('Ymd'));
+
+        $this->assertSame($expectedSoa, $soaData[0]->getRecords()[0]->getContent());
     }
 }
