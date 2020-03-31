6df76cb7ea33400e45a70b499f2d5a30e42009f8
Tristan <t.siebers@exonet.nl>
2020-03-31 12:41:06 +0200

    Update / add tests

---
+++ tests/functional/AdvancedZoneCreationTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Exonet\Powerdns\tests\functional;
+
+use Exonet\Powerdns\Powerdns;
+use Exonet\Powerdns\RecordType;
+use Exonet\Powerdns\Resources\Zone as ZoneResource;
+use PHPUnit\Framework\TestCase;
+
+class AdvancedZoneCreationTest extends TestCase
+{
+    public function testCreateSoaIncrementZone(): void
+    {
+        $powerdns = new Powerdns('127.0.0.1', 'apiKey');
+        // Create a unique zone/domain name.
+        $canonicalDomain = 'advanced-zone.'.time().'.test.';
+
+        // Create a new zone resource.
+        $newZone = new ZoneResource();
+        $newZone->setName($canonicalDomain);
+        $newZone->setKind('native');
+        $newZone->setDnssec(true);
+        $newZone->setSoaEdit('epoch');
+        $newZone->setSoaEditApi('epoch');
+        $newZone->setNameservers(['ns1.test.']);
+        $newZone->setApiRectify(false);
+        $newZone->setNsec3param('1 0 100 1234567890');
+
+        // Create a new zone with the defined records and name servers.
+        $powerdns->createZoneFromResource($newZone);
+
+        // Get the zone and check the results.
+        $zone = $powerdns->zone($canonicalDomain);
+        $zoneResource = $zone->resource();
+        $this->assertSame($canonicalDomain, $zone->getCanonicalName());
+        $this->assertSame($canonicalDomain, $zoneResource->getName());
+        $this->assertSame('Native', $zoneResource->getKind());
+        $this->assertTrue($zoneResource->hasDnssec(), 'DNSSEC not correctly set.');
+        $this->assertSame('EPOCH', $zoneResource->getSoaEdit());
+        $this->assertSame('EPOCH', $zoneResource->getSoaEditApi());
+        $this->assertSame(['ns1.test.'], $zoneResource->getNameservers());
+        $this->assertFalse($zoneResource->hasAutoRectify(), 'API Rectify not correctly set.');
+        $this->assertSame('1 0 100 1234567890', $zoneResource->getNsec3param());
+    }
+}
