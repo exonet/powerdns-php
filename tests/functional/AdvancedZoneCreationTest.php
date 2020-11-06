fc9bc83a8939d249bcec4bb7ddacc6d83dfabaaf
Styxit <info@styxit.com>
2020-11-06 14:04:48 +0100

    Create new zone with account

--- tests/functional/AdvancedZoneCreationTest.php
+++ tests/functional/AdvancedZoneCreationTest.php
@@ -22,6 +22,7 @@ class AdvancedZoneCreationTest extends FunctionalTestCase
         $newZone->setNameservers(['ns1.test.']);
         $newZone->setApiRectify(false);
         $newZone->setNsec3param('1 0 100 1234567890');
+        $newZone->setAccount('account-name');
 
         // Create a new zone with the defined records and name servers.
         $this->powerdns->createZoneFromResource($newZone);
@@ -38,5 +39,6 @@ class AdvancedZoneCreationTest extends FunctionalTestCase
         $this->assertSame(['ns1.test.'], $zoneResource->getNameservers());
         $this->assertFalse($zoneResource->hasAutoRectify(), 'API Rectify not correctly set.');
         $this->assertSame('1 0 100 1234567890', $zoneResource->getNsec3param());
+        $this->assertSame('account-name', $zoneResource->getAccount());
     }
 }
