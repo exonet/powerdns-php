648cc2b4dd3f72613090b7bc517333174b51ed55
Jeroen Vermeulen - MageHost <jeroen@magehost.pro>
2023-02-07 00:06:56 +0100

    Implemented Zone::setKind

---
+++ src/Transformers/KindTransformer.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Exonet\Powerdns\Transformers;
+
+class KindTransformer extends Transformer
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function transform()
+    {
+        $result = (object) [
+            'kind' => $this->data['kind'],
+        ];
+        if ('Slave' == $this->data['kind']) {
+            $result->masters = $this->data['masters'];
+        }
+        return $result;
+    }
+}
