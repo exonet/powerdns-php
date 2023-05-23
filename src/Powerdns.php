60ba176f3d094d450d35511b5e26c9e5cd47bd1c
trizz <trizz@users.noreply.github.com>
2023-05-23 08:40:14 +0000

    Auto update drafter version in CLIENT_VERSION

--- src/Powerdns.php
+++ src/Powerdns.php
@@ -15,7 +15,7 @@ class Powerdns implements PowerdnsInterface
     /**
      * The version of this package. This is being used for the user-agent header.
      */
-    public const CLIENT_VERSION = 'v4.3.1';
+    public const CLIENT_VERSION = 'v4.4.0';
 
     /**
      * @var Powerdns The client instance.
